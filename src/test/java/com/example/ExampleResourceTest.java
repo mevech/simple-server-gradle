c7f84c947fc37affc0aba19902187b065b98a437
mevech <eve.m.chen@gmail.com>
2020-12-06 09:01:52 +0100

    test: add integration test
    apparently, @QuarkusTest and @Nested can not be combined. so integration
    test will take a little longer

--- src/test/java/com/example/ExampleResourceTest.java
+++ src/test/java/com/example/ExampleResourceTest.java
@@ -1,21 +1,103 @@
 package com.example;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class ExampleResourceTest {
-
+    static final String BASE_URL = "http://localhost:8080/message-board";
     @Test
-    public void testHelloEndpoint() {
+    @Order(0)
+    @DisplayName("Insert a new message with client should succeed")
+    public void testPostHelloEndpoint() {
         given()
-          .when().get("/resteasy/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+                .when()
+                .cookie("clientId", "client0")
+                .body("message")
+                .post("/message-board")
+                .then()
+                .statusCode(201);
     }
+    @Nested
+    @QuarkusTest
+    @ApplicationScoped
+    class testEmptyUpdate {
+        @Test
+        @Order(0)
+        @DisplayName("Insert a new message with client should succeed")
+        public void testPostHelloEndpoint() {
+            given()
+                .when()
+                    .cookie("clientId", "client0")
+                    .body("message")
+                    .post("/message-board")
+                .then()
+                    .statusCode(201);
+        }
+
+        @Test
+        @Order(1)
+        @DisplayName("Insert a new message with client should succeed")
+        public void print() {
+            given()
+                .when()
+                    .cookie("clientId", "client0")
+                    .get("/message-board").body().prettyPrint();
+
+        }
+    }
+
+    @Nested
+    @QuarkusTest
+    class testUpdateAfterInsert {
+        @Test
+        @Order(0)
+        @DisplayName("Insert a new message with client should succeed")
+        public void testInsert() {
+            given()
+                .when()
+                    .cookie("clientId", "client0")
+                    .body("message")
+                    .post("/message-board")
+                .then()
+                    .statusCode(201);
+        }
+
+        @Test
+        @Order(1)
+        @DisplayName("Insert a new message with client should succeed")
+        public void print() {
+            given()
+                    .when()
+                    .cookie("clientId", "client0")
+                    .get("/message-board").body().prettyPrint();
+
+        }
+    }
+
+    @Nested
+    @QuarkusTest
+    class Flow1 {
+        @Test
+        @DisplayName("Insert a new message with client should succeed")
+        public void testPostHelloEndpoint() {
+            given()
+                    .when()
+                    .cookie("clientId", "client0")
+                    .body("message")
+                    .post("/message-board")
+                    .then()
+                    .statusCode(201);
+        }
+
+    }
+
 
 }
